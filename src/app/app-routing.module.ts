08139625f82f2a69fea43c295cb2331719193d62
amr ahmed <micro.genius2010@gmail.com>
2022-07-17 02:05:43 +0200

    fetch products as service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './components/cart/cart.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 const routes: Routes = [
-  {path:'cart', component: CartComponent}
+  {path:'cart', component: CartComponent},
+  {path:'home', component: ProductListComponent},
+  {path:'product/:id', component: ProductItemDetailComponent}
 ];
 
 @NgModule({
